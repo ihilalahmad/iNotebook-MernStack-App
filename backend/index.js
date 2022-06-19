c7683d97eeb07c872933ff237237c3ca3db3c436
Hilal Ahmad <ihilalahmadd@gmail.com>
2022-06-19 13:46:40 +0500

    backend changes

--- backend/index.js
+++ backend/index.js
@@ -10,5 +10,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`inotebook app listening on port http://localhost:${port}`);
+  console.log(`inotebook app listening on port https://localhost:${port}`);
 });
