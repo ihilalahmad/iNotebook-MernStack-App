6e02e8efe79a5dd5b7b5c1c44101eeb78e7050ec
Hilal Ahmad <ihilalahmadd@gmail.com>
2022-06-22 14:30:39 +0500

    user signup & login completed/

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,9 +4,11 @@ const router = express.Router();
 const { body, validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const fetchuser = require("../middleware/fetchuser");
 
 const JWT_SECRET = "inotebookisgoodapp";
 
+//ROUTE:1 create a user by using POST method. "/api/auth/createuser"
 router.post(
   "/createuser",
   [
@@ -51,10 +53,69 @@ router.post(
       res.send({ authtoken });
     } catch (error) {
       console.log(error.message);
-      res.status(500).send("Some error occured");
+      res.status(500).send("Internal server error");
     }
   }
 );
-//this is test comment
+
+//ROUTE:2 authenticate a user by using POST method. "api/auth/login"
+router.post(
+  "/login",
+  [
+    //email should be type email
+    body("email", "Enter a valid email").isEmail(),
+    //password cannot be blank
+    body("password", "Password cannot be blank").exists(),
+  ],
+  async (req, res) => {
+    //if there errors return a bad req and the errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+
+      if (!user) {
+        return res
+          .status(400)
+          .json({ error: "Please try to login with correct credentials" });
+      }
+      //comparing user entered password with db password
+      const comparePassword = await bcrypt.compare(password, user.password);
+
+      if (!comparePassword) {
+        return res
+          .status(400)
+          .json({ error: "Please try to login with correct credentials" });
+      }
+
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const authtoken = jwt.sign(data, JWT_SECRET);
+      res.json({ authtoken });
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).send("Internal server error");
+    }
+  }
+);
+
+//ROUTE 3: get user detials using GET method. "/api/auth/getuser"
+router.post("/getuser", fetchuser, async (req, res) => {
+  try {
+    userId = req.user.id;
+    const user = await User.findById(userId).select("-password");
+    res.send(user);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send("Internal server error");
+  }
+});
 
 module.exports = router;
--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,13 +1,52 @@
 const express = require("express");
 const router = express.Router();
+const fetchuser = require("../middleware/fetchuser");
+const Notes = require("../models/Notes");
+const { body, validationResult } = require("express-validator");
+const { json } = require("express");
 
-router.get("/", (req, res) => {
-  obj = {
-    title: "this is the title of note",
-    description: "this is the default description of note",
-    tag: "this is the tag of this note",
-  };
-  res.json(obj);
+//ROUTE 1: Get all the notes using GET "/api/notes/fetchallnotes". login required
+router.get("/fetchallnotes", fetchuser, (req, res) => {
+  try {
+    const notes = await.Notes.find({ user: req.user.id });
+    res.json(notes);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send("Internal server error");
+  }
 });
+
+//ROUTE 2: Add a new note using POST "/api/notes/addnote". login required
+router.post(
+  "/addnote",
+  fetchuser,
+  [
+    body("title", "Enter a valid title").isLength({ min: 3 }),
+    body("description", "Description must be at least 5 characters").isLength({
+      min: 5,
+    }),
+  ],
+  async (req, res) => {
+    try {
+      const { title, description, tag } = req.body;
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+      const note = new Notes({
+        title,
+        description,
+        tag,
+        user: req.user.id,
+      });
+
+      const savedNote = await note.save();
+      res.json(savedNote);
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).send("Internal server error");
+    }
+  }
+);
 
 module.exports = router;
